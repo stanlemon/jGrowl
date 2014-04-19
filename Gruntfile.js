44c07b6d2909050537289cb730900df9349346db
Stan Lemon <stosh1985@gmail.com>
2014-04-18 22:37:19 -0400

    Removing banner and updating generated files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! jquery.jgrowl.js <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         sourceMap: true,
         sourceMapName: 'jquery.jgrowl.map'
       },
