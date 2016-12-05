8a0065f2a6f037fcac163a22b073e6bdb982a2e5
Kael Santini <kaelsantini@hotmail.com>
2016-12-05 14:20:06 -0300

    Closer template bug
    Fixing the bug that closerTemplate property did not apply to plugin.

--- jquery.jgrowl.js
+++ jquery.jgrowl.js
@@ -328,7 +328,7 @@
 			/** Add a Global Closer if more than one notification exists **/
 			if ($('.jGrowl-notification:parent', self.element).length > 1 &&
 				$('.jGrowl-closer', self.element).length === 0 && this.defaults.closer !== false ) {
-				$(this.defaults.closerTemplate).addClass('jGrowl-closer ' + this.defaults.themeState + ' ui-corner-all').addClass(this.defaults.theme)
+				$(o.closerTemplate).addClass('jGrowl-closer ' + this.defaults.themeState + ' ui-corner-all').addClass(this.defaults.theme)
 					.appendTo(self.element).animate(this.defaults.animateOpen, this.defaults.speed, this.defaults.easing)
 					.bind("click.jGrowl", function() {
 						$(this).siblings().trigger("jGrowl.beforeClose");
