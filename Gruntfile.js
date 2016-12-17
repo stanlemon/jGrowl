d31b5d11038d04806b8d5f39f0cdd2db9b65fb63
Patrik Laszlo <alabard@gmail.com>
2016-12-17 14:25:48 +0100

    quick fix.

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,7 @@ module.exports = function(grunt) {
         test: {
             options: {
                 reporter: 'spec',
-                captureFile: 'results.txt', // Optionally capture the reporter output to a file
+//                captureFile: 'results.txt', // Optionally capture the reporter output to a file
                 quiet: false, // Optionally suppress output to standard out (defaults to false)
                 clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
                 noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
